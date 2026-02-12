d9bfd410bcf502c8e78260122252c5ef5fad3534
kapadiya12 <kapadiyakrish608@gmail.com>
2026-02-12 17:40:38 +0530

    secure render deployment

--- shopconfig/settings.py
+++ shopconfig/settings.py
@@ -20,13 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ecy*l-kc=0@^h4vk^qq6!cg)8t(0_sx)5ch4#1(qptp*p9gylg'
+SECRET_KEY = os.environ.get("django-insecure-ecy*l-kc=0@^h4vk^qq6!cg)8t(0_sx)5ch4#1(qptp*p9gylg")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
+import os
 
 # Application definition
 
@@ -37,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop',
+    'shop','whitenoise.runserver_nostatic',
+
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -77,9 +80,13 @@ WSGI_APPLICATION = 'shopconfig.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": "5432",
     }
 }
 
@@ -136,4 +143,12 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'king143krish@gmail.com'
-EMAIL_HOST_PASSWORD = 'nwkcnfzzsfygmkme'
+# EMAIL_HOST_PASSWORD = 'nwkcnfzzsfygmkme'
+EMAIL_HOST_PASSWORD = os.environ.get("nwkcnfzzsfygmkme")
+
+
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
